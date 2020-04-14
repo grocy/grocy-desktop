6ab077734a4c3de4d77d92a45e51c4070d109992
Bernd Bestel <bernd@berrnd.de>
2020-04-14 21:00:01 +0200

    Set the env var CURL_ALLOW_INSECURE_SSL_CA for Barcode Buddy php process (references #13)

--- grocy-desktop/BarcodeBuddyEnvironmentManager.cs
+++ grocy-desktop/BarcodeBuddyEnvironmentManager.cs
@@ -22,12 +22,14 @@ namespace GrocyDesktop
 			{
 				Directory.CreateDirectory(this.DataPath);
 			}
+
 			// Dummy API key, not needed, due to disabled authentication, but required
 			this.SetSetting("OVERRIDDEN_USER_CONFIG", "GROCY_API_URL="+grocyApiUrl+";GROCY_API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 			this.SetSetting("DISABLE_AUTHENTICATION", "true");
 			this.SetSetting("CONFIG_PATH", Path.Combine(this.DataPath, "config.php"));
 			this.SetSetting("AUTHDB_PATH", Path.Combine(this.DataPath, "users.db"));
 			this.SetSetting("DATABASE_PATH", Path.Combine(this.DataPath, "barcodebuddy.db"));
+			this.SetSetting("CURL_ALLOW_INSECURE_SSL_CA", "true");
 
 			File.Copy(Path.Combine(this.BasePath, "config-dist.php"), Path.Combine(this.DataPath, "config.php"), true);
 		}
